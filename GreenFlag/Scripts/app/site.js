c83da3acf7290ee2c170e03f8a2ca247427ee19b
Chris McGinn <chris.s.mcginn@live.com>
2014-03-30 11:53:51 -0400

    Before Content Form

---
+++ GreenFlag/Scripts/app/site.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    if ($('#bannerImageRotator').length) {
+
+        $('#bannerImageRotator').flexslider({
+            animation: "fade",
+            controlNav: false,
+            directionNav: false,
+            touch: true,
+            direction: "vertical",
+            animationSpeed: 1600
+        });
+    }
+})
